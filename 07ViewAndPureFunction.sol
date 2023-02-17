4c1a07d838570449aa2b47ea90c3320760855be0
Umesh Singh Mehta <30582926+usmfit@users.noreply.github.com>
2023-02-17 17:17:39 +0530

    Create 07ViewAndPureFunction.sol

---
+++ 07ViewAndPureFunction.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// View Function can read data from the Blockchain whereas Pure Function donot read
+// from Blockchain
+
+contract ViewAndPureFunctions {
+    uint public num;
+
+    function viewFunc() external view returns (uint) {
+        return num;
+    }
+
+    function pureFunc() external pure returns (uint) {
+        return 1;
+    }
+
+    function addToNum(uint x) external view returns (uint) {
+        return num + x;
+    }
+
+    function add(uint x, uint y) external pure returns (uint) {
+        return x + y;
+    }
+}
